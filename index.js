c293df20f82af6cd8aafe9026f571e81cf418c33
Jay <butchich73@gmail.com>
2024-06-26 17:05:08 -0700

    Version1

---
+++ index.js
@@ -0,0 +1,19 @@
+function sum(a, b) {
+    return  a + b
+}
+
+
+const people = {
+    Name : "Jay",
+    Age: 50,
+    Gender: "Male"
+}
+let i;
+for(i=0; i <= 10; i++){
+    console.log(i)
+}
+
+
+console.log(Object.values(people))
+console.log(sum(1,5))
+
